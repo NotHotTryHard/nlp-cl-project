746327c725fbb8d4753ff4ed88655f1436f0f87b
NoMoreActimel <evolution3000futurama@gmail.com>
2023-12-03 01:50:26 +0300

    removed augmentations

--- src/augmentations/spectrogram_augmentations/__init__.py
+++
@@ -1,9 +0,0 @@
-from src.augmentations.spectrogram_augmentations.FreqMasking import FreqMaskingSpecAug
-from src.augmentations.spectrogram_augmentations.TimeMasking import TimeMaskingSpecAug
-from src.augmentations.spectrogram_augmentations.TimeStretch import TimeStretchSpecAug
-
-__all__ = [
-    "FreqMaskingSpecAug",
-    "TimeMaskingSpecAug",
-    "TimeStretchSpecAug"
-]
--- src/augmentations/wave_augmentations/__init__.py
+++
@@ -1,7 +0,0 @@
-from src.augmentations.wave_augmentations.Gain import Gain
-from src.augmentations.wave_augmentations.PitchShift import PitchShift
-
-__all__ = [
-    "Gain",
-    "PitchShift"
-]
