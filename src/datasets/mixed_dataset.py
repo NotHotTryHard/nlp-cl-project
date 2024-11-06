f4b91c74495bfd6e089105131617f8a8319af5b5
NoMoreActimel <evolution3000futurama@gmail.com>
2024-11-06 10:44:00 -0500

    small mixing dataset fix

--- src/datasets/mixed_dataset.py
+++ src/datasets/mixed_dataset.py
@@ -44,9 +44,12 @@ class MixedSequentialDataset(TorchDataset):
 
         self.cum_sequential_length = np.cumsum([len(dataset) for dataset in self.sequential_datasets])
         self.replacements_counter = 0
+
+        self.current_dataset = 0
     
     def __len__(self):
-        return sum(len(dataset) for dataset in self.sequential_datasets)
+        # return sum(len(dataset) for dataset in self.sequential_datasets)
+        return len(self.sequential_datasets[self.current_dataset])
 
     def __getitem__(self, idx):
         if self.base_dataset is not None and self.base_mixing_rate:
@@ -57,7 +60,8 @@ class MixedSequentialDataset(TorchDataset):
                 return self.base_dataset[base_idx]
     
         real_idx = idx - self.replacements_counter
-        n_prev_datasets = next(i for i, length in enumerate(self.cum_sequential_length) if length > real_idx)
+        n_prev_datasets = self.current_dataset
+        # n_prev_datasets = next(i for i, length in enumerate(self.cum_sequential_length) if length > real_idx)
         prev_length = self.cum_sequential_length[n_prev_datasets - 1] if n_prev_datasets else 0
 
         if self.sequential_mixing_rate and n_prev_datasets:
@@ -68,7 +72,7 @@ class MixedSequentialDataset(TorchDataset):
                 seq_idx = np.random.randint(0, len(self.sequential_datasets[dataset_idx]))
                 return self.sequential_datasets[dataset_idx][seq_idx]
         
-        return self.sequential_datasets[n_prev_datasets][real_idx - prev_length]
+        return self.sequential_datasets[n_prev_datasets][real_idx] # (real_idx - prev_length) % len(self.current_dataset)]
 
 
 class MixedSequentialDatasetCategorical(TorchDataset):
