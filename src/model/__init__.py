8ec774e32ea59becb0f55f4e75e913fc272260e0
Petr Skovorodnikov <peter.skovorodnikov@gmail.com>
2024-12-04 02:35:35 -0500

    svd lora

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,9 +1,11 @@
 from src.model.model import DecoderModel
 from src.model.t5model import T5forSummarization
 from src.model.t5_lora_model import T5LoRA
+from src.model.t5_svd_lora_model import T5SVDLoRA
 
 __all__ = [
     "DecoderModel",
     "T5forSummarization",
-    "T5LoRA"
+    "T5LoRA",
+    "T5SVDLoRA"
 ]
