40aec21f228fc71e2318760e4e24d58fb718b50e
NoMoreActimel <evolution3000futurama@gmail.com>
2024-12-10 11:30:17 -0500

    seq-svd-lora fixed+reversed, wandb run editing

--- src/model/t5_svd_lora_sequential_model.py
+++ src/model/t5_svd_lora_sequential_model.py
@@ -17,7 +17,7 @@ class SVDLoRASequential(nn.Module):
         self.loras_s = nn.ParameterList()
         self.loras_vt = nn.ParameterList()
 
-        for i in range(n_adapters):
+        for i in range(n_adapters - 1, -1, -1):
             start_index = -rank * (i + 1)
             end_index = -rank * i if i else self.u.shape[1]
             self.loras_u.append(nn.Parameter(self.u[:, start_index:end_index], requires_grad=False)) # out_features, rank
@@ -28,21 +28,41 @@ class SVDLoRASequential(nn.Module):
         self.s = nn.Parameter(self.s[:-rank * n_adapters], requires_grad=False)
         self.vt = nn.Parameter(self.vt[:-rank * n_adapters, :], requires_grad=False)
 
-        self.register_buffer('orig_weight', self.u @ torch.diag(self.s) @ self.vt)
+        self.register_buffer('orig_weight', self.u @ torch.diag(self.s) @ self.vt + sum(
+            lora_u @ torch.diag(lora_s) @ lora_vt
+            for lora_u, lora_s, lora_vt in zip(self.loras_u, self.loras_s, self.loras_vt)
+        ))
 
         self.extra_loss = torch.tensor(0., device=self.u.device)
         self.enable_extra_loss = enable_extra_loss
     
-        self.current_adapter_index = 0
+        self.cur_idx = -1
         self.update_adapter(0)
     
-    def update_adapter(self, current_adapter_index):
-        self.current_adapter_index = current_adapter_index
-        for i in range(self.n_adapters):
-            requires_grad = (i == self.current_adapter_index)
-            self.loras_u[i].requires_grad = requires_grad
-            self.loras_s[i].requires_grad = requires_grad
-            self.loras_vt[i].requires_grad = requires_grad
+    def update_adapter(self, new_idx):            
+        if new_idx != self.cur_idx:
+            if self.cur_idx != -1:
+                prev_lora_u = self.loras_u[self.cur_idx]
+                prev_lora_s = self.loras_s[self.cur_idx]
+                prev_lora_vt = self.loras_vt[self.cur_idx]
+                
+                for lora_part in [prev_lora_u, prev_lora_s, prev_lora_vt]:
+                    lora_part.requires_grad = False
+                    
+                prev_lora_weight = (prev_lora_u @ torch.diag(prev_lora_s) @ prev_lora_vt).detach().clone()
+                self.orig_weight.data = self.orig_weight + prev_lora_weight
+
+            new_lora_u = self.loras_u[new_idx]
+            new_lora_s = self.loras_s[new_idx]
+            new_lora_vt = self.loras_vt[new_idx]
+            
+            new_lora_weight = (new_lora_u @ torch.diag(new_lora_s) @ new_lora_vt).detach().clone()
+            self.orig_weight.data = self.orig_weight - new_lora_weight
+            
+            for lora_part in [new_lora_u, new_lora_s, new_lora_vt]:
+                lora_part.requires_grad = True
+            
+            self.cur_idx = new_idx
         
     def calc_extra_loss(self):
         u_cat = torch.cat([self.u, *[lora_u for lora_u in self.loras_u]], 1)
@@ -55,15 +75,29 @@ class SVDLoRASequential(nn.Module):
         self.extra_loss = torch.tensor(0., device=self.u.device)
     
     def forward(self, x):
-        output = x @ self.orig_weight
-        for i in range(self.n_adapters):
-            lora_output = x @ self.loras_u[i] @ torch.diag(self.loras_s[i]) @ self.loras_vt[i]
-            output = output + lora_output
+        lora_u = self.loras_u[self.cur_idx]
+        lora_s = self.loras_s[self.cur_idx]
+        lora_vt = self.loras_vt[self.cur_idx]
+        
+        output = x @ (self.orig_weight + lora_u @ torch.diag(lora_s) @ lora_vt)
 
         if self.enable_extra_loss:
             self.calc_extra_loss()
         
         return output
+
+    def to(self, device, **kwargs):
+        # Move only the current adapter to the target device
+        self.u.data = self.u.data.to(device, **kwargs)
+        self.s.data = self.s.data.to(device, **kwargs)
+        self.vt.data = self.vt.data.to(device, **kwargs)
+        self.orig_weight = self.orig_weight.to(device, **kwargs)
+        
+        for i in range(self.n_adapters):
+            for loras_submodules in [self.loras_u, self.loras_s, self.loras_vt]:
+                loras_submodules[i].data = loras_submodules[i].data.to(device, **kwargs)
+        
+        return self
 
 
 class T5SVDLoRASequential(T5AdapterBase):
@@ -83,15 +117,14 @@ class T5SVDLoRASequential(T5AdapterBase):
         
         for name, module in self.named_modules():
             if self.check_module(name, module):
-                for i in range(self.n_adapters):
-                    module.lora = SVDLoRASequential(module, enable_extra_loss=True, **svd_lora_config)
-                    module.forward = self.add_lora_forward(module)
-                    self.svd_loras.append(module.lora)
-                    self.count_adaptable_weights += 2
+                module.lora = SVDLoRASequential(module, enable_extra_loss=True, **svd_lora_config)
+                module.forward = self.add_lora_forward(module)
+                self.svd_loras.append(module.lora)
+                self.count_adaptable_weights += 2
         
         self.disabled_adapters = False
         self.update_adapters(adapter_idx=0)
-        print("Init loss:", self.calc_extra_loss())
+        # print("Init loss:", self.calc_extra_loss())
 
     def check_module(self, name, module):
         return isinstance(module, nn.Linear) and name.split('.')[-1] in self.svd_lora_config['target_layers']
@@ -125,7 +158,7 @@ class T5SVDLoRASequential(T5AdapterBase):
                 module.lora.enable_extra_loss = False
             
     def collect_extra_loss(self):
-        extra_loss = torch.tensor(0., device=self.model.device)
+        extra_loss = torch.tensor(0., device=self.svd_loras[0].u.device)
         for svd_lora in self.svd_loras:
             extra_loss = extra_loss + svd_lora.extra_loss
             svd_lora.clean_extra_loss()
@@ -134,7 +167,7 @@ class T5SVDLoRASequential(T5AdapterBase):
         return extra_loss
 
     def calc_extra_loss(self):
-        extra_loss = torch.tensor(0., device=self.model.device)
+        extra_loss = torch.tensor(0., device=self.svd_loras[0].u.device)
         for svd_lora in self.svd_loras:
             svd_lora.calc_extra_loss()
             extra_loss = extra_loss + svd_lora.extra_loss
@@ -152,3 +185,21 @@ class T5SVDLoRASequential(T5AdapterBase):
             module.original_forward = module.forward
         
         return new_forward
+
+    def to(self, device, **kwargs):
+        for name, param in self.named_parameters():
+            if not "lora" in name:
+                if param.data.device != device:
+                    param.data = param.data.to(device, **kwargs)
+                    if param.grad is not None:
+                        param.grad.data = param.grad.data.to(device, **kwargs)
+
+        for buffer_name, buffer in self.named_buffers():
+            if buffer.device != device:
+                print(buffer_name)
+                setattr(self, buffer_name, buffer.to(device, **kwargs))
+
+        for svd_lora in self.svd_loras:
+            svd_lora.to(device, **kwargs)
+        
+        return self
