e25032f0a5d2d6fc3275e596488f18979778a1cb
NoMoreActimel <evolution3000futurama@gmail.com>
2025-07-03 21:29:30 -0400

    datasets init updated

--- src/datasets/__init__.py
+++ src/datasets/__init__.py
@@ -4,6 +4,8 @@ from src.datasets.mixed_dataset import MixedSequentialDataset
 from src.datasets.huggingface_dataset import HuggingFaceDataset
 from src.datasets.math_datasets import MathQADataset, MathDataset
 from src.datasets.lpips_dataset import LPIPSReorderedDataset
+from src.datasets.glue_dataset import GLUEHuggingFaceDataset, SuperGLUEHuggingFaceDataset
+from src.datasets.mlqa_dataset import MLQAHuggingFaceDataset
 
 __all__ = [
     "OriginalDataset",
@@ -12,5 +14,8 @@ __all__ = [
     "HuggingFaceDataset",
     "MathQADataset",
     "MathDataset",
-    "LPIPSReorderedDataset"
+    "LPIPSReorderedDataset",
+    "GLUEHuggingFaceDataset",
+    "SuperGLUEHuggingFaceDataset",
+    "MLQAHuggingFaceDataset"
 ]
